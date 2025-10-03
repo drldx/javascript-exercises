5aba1cb72d57d5c03a4b7f33d04bdaee1fc80ce8
drldx <168914904+drldx@users.noreply.github.com>
2025-10-03 15:32:25 +0530

    update: did workouts with repeatString.js
    updated the code in repeatString.js to pass the test and removed the
    .skip from spec.js to pass all the 8 tests, "npm test <filename>" used;

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if(num === 0) return '';
+  if(num < 0) return 'ERROR';
 
+  let Str2 = "";
+  while (num){
+    Str2 += string;
+    num --;
+  }
+  return Str2;
 };
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
