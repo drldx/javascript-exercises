f7a62cf3368090c13496d274011f642fda11b410
drldx <168914904+drldx@users.noreply.github.com>
2025-10-03 15:52:51 +0530

    update: reverseString.js updated with function;
    wrote a function to reverse a given string by splitting it into a
    new array and returning it by
    concatinating each letter from the end to a new string.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let len = string.length - 1;
+  let stringArr = string.split("");
+  let reverse = ""; 
+  while(len >= 0){
+   reverse += stringArr[len]; 
+    len--;
+  }
+  return reverse; 
 };
 
 // Do not edit below this line
