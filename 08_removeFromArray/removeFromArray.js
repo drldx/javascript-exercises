5e982b37ff1278eb8579228f517f922dce92269d
drldx <168914904+drldx@users.noreply.github.com>
2025-10-03 18:06:43 +0530

    update: 08 & 09 solutions

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr = [], ...items) {
+  let args = items;  
+
+  for(let i = 0; i < args.length; i++ ){
+    let index = 0;
+    while (arr.includes(args[i])) {
+      index = arr.indexOf(args[i]);
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if((num1 < 0 || num2 < 0)) return 'ERROR';
+  if(!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
 
+  let first = (num1 > num2) ? num2 : num1 ;
+  let last = (num1 < num2) ? num2 : num1;
+  let sumArr = [];
+  while(first <= last){
+    sumArr.push(first++);
+  }
+  return sumArr.reduce((a, b) => (a + b));
 };
 
 // Do not edit below this line
