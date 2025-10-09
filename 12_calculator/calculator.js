e60958a5e130a67fc63f9a403717fa6d8090655c
drldx <168914904+drldx@users.noreply.github.com>
2025-10-09 14:51:16 +0530

    update: calculator functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, curr) => acc + curr , 0);	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let res = 1;
+  while(exponent){
+    res *= base; 
+    exponent --;
+  }
+  return res;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+  while (num) {
+    res *= num ;
+    num--; 
+  }
+  return res;
 };
+
 
 // Do not edit below this line
 module.exports = {
