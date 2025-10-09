3e5381f5eb19062f2fc0821f5ce4aa9360eb9b6e
drldx <168914904+drldx@users.noreply.github.com>
2025-10-09 15:51:20 +0530

    add: solution for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const cleanWord = word.replaceAll(/[^a-zA-Z0-9]/g, "" ).toLowerCase();
+  const reversed = cleanWord.split("").reverse().join("");
 
+  return cleanWord === reversed ? true : false ;
 };
 
 // Do not edit below this line
