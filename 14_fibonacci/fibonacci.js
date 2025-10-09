39003665d8875f859f922765b28f341fb77fa990
drldx <168914904+drldx@users.noreply.github.com>
2025-10-09 16:39:35 +0530

    add: fibonacci solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if(num < 0) return "OOPS";
+  if( num == 0) return 0;
+  
+  let arr = [];
 
+  for(let i = 0 ; i < num; i++){
+    if(i === 0 || i === 1){
+      arr[i] = 1;
+    } 
+    if(i > 1){
+      arr[i] = (arr[i-1] + arr[i-2]);
+    }
+  }
+  return arr[num-1]; 
 };
 
 // Do not edit below this line
