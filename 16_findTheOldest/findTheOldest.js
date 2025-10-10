c917221dec1608d1d8f87b330103c60cddfd9b56
drldx <168914904+drldx@users.noreply.github.com>
2025-10-10 12:56:14 +0530

    update: findTheOldest solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
+  const year = new Date().getFullYear();
+  
+  const newArray = peoples.map(item => {
+    if(!item.yearOfDeath){
+      item.yearOfDeath = year;
+    }
+      return item;
+  } );
 
+  const res = newArray.sort((a,b) => ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) ? 1 : -1);
+
+  return res[0];
 };
 
 // Do not edit below this line
